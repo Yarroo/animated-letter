d204931a8de3e0add308cc56fdf86abb108c49c9
Sergey Vershinin <accountwm@gmail.com>
2019-07-01 12:54:34 +0300

    order animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,39 @@
-$( function() {
-  $( '.type-text' ).each( function() {
-    var items = $( this ).text();
-    $( this ).empty().teletype( {
-      text: $.map( items.split( ';' ), $.trim ),
+function WriteAnimation (text, class_name, delay = 0 , callback = null) {
+  console.log("1")
+  $(class_name).empty().teletype( {
+      text: text,
       typeDelay: 5,
       backDelay: 20,
       cursor: false,
       preserve: true,
       humanise: true,
-      loop: 1
+      loop: 1,
+      delay: delay,
+      callbackFinished: function( teletype ) {
+        if (callback != null) {
+          callback();}
+      }
+    }
+  );
+}
 
-    } );
-  } );
-} );
+function WriteHeader() {
+  text = "Привет, Ирина Марковна!";
+  class_name = ".letter-title";
+  WriteAnimation(text, class_name, 20, WriteBody);
+}
+
+function WriteBody() {
+  text = "Пишет Вам брат Лука Сергеевич Сотников.";
+  class_name = ".letter-body";
+  WriteAnimation(text, class_name, 20, WriteFooter);
+}
+
+function WriteFooter() {
+  text = "Ваш Лука Сотников";
+  class_name = ".letter-footer";
+  WriteAnimation(text, class_name, 20, );
+}
+
+
+document.addEventListener("DOMContentLoaded", WriteHeader);
